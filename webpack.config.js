84da549c8a80f1b3664f80daa943e27bf1f76977
Matt Lawrence <matlaw@pennmedicine.upenn.edu>
2018-09-28 14:17:48 -0400

    Configure webpack to properly imbibe assets

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,47 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-	entry: {
-		app: './src/index.js'
-	},
-	output: {
-		filename: '[name].bundle.js',
-		path: path.resolve(__dirname, 'dist')
-	},
-	module: {
-		rules : [
+  entry: {
+    bert: './src/index.js'
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
 			//js
-	    {
-	      test: /\.js$/,
-	      exclude: /(node_modules|bower_components)/,
-	      use: {
-	        loader: 'babel-loader',
-	        options: {
-	          presets: ['env']
-	        }
-	      }
-	    }
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+        }
+	    },
+      //scss
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader']
+        })
+      }
 	  ]
-	}
+  },
+  plugins: [
+    new ExtractTextPlugin({
+      filename: 'bert.css'
+    }),
+    new HtmlWebpackPlugin({
+      inject: false,
+      hash: true,
+      template: './src/index.html',
+      filename: 'index.html'
+    })
+  ]
 }
